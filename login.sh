f34c9e2005b4d89e0fa2483d0dd5a62a6fe33bf1
Jay Shah <hey@jay.sh>
2014-08-23 12:59:21 +0100

    Make the mouse settings Ubuntu-variant specific.

--- login.sh
+++ login.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-xinput --set-prop "CyPS/2 Cypress Trackpad" "Device Accel Velocity Scaling" 50
-xinput --set-prop "CyPS/2 Cypress Trackpad" "Synaptics Scrolling Distance" 8 8
+set -x
+if [ -d /etc/linuxmint ];
+then
+	# On Linux Mint, the defaults result in movement that is too fast to manage.
+	xinput --set-prop "CyPS/2 Cypress Trackpad" "Device Accel Velocity Scaling" 30
+	xinput --set-prop "CyPS/2 Cypress Trackpad" "Synaptics Scrolling Distance" 10 10
+else
+	xinput --set-prop "CyPS/2 Cypress Trackpad" "Device Accel Velocity Scaling" 50
+	xinput --set-prop "CyPS/2 Cypress Trackpad" "Synaptics Scrolling Distance" 8 8
+fi
